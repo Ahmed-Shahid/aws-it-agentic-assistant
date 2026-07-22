af233af304cb9e4377f9b142606b61fb42cab52c
Ahmed-Shahid <ahmed.s.shahid@gmail.com>
2026-07-22 16:25:32 -0700

    Added comments in stack as placeholders for resources

--- aws_it_agentic_assistant/aws_it_agentic_assistant_stack.py
+++ aws_it_agentic_assistant/aws_it_agentic_assistant_stack.py
@@ -2,9 +2,13 @@ from constructs import Construct
 from aws_cdk import (
     Duration,
     Stack,
+    CfnOutput,
     aws_lambda as _lambda,
     aws_lambda_event_sources as lambda_event_sources,
-    aws_secretsmanager as secretsmanager
+    aws_secretsmanager as secretsmanager,
+    aws_stepfunctions as sfn,
+    aws_stepfunctions_tasks as tasks,
+    aws_iam as iam,
 )
 
 class AwsItAgenticAssistantStack(Stack):
@@ -12,7 +16,9 @@ class AwsItAgenticAssistantStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # SECRETS
+        '''
+        SECRETS
+        '''
         jira_secret = secretsmanager.Secret(
             self, "JiraSecret",
             secret_name="jira_token"
@@ -23,9 +29,14 @@ class AwsItAgenticAssistantStack(Stack):
             secret_name="claude_api_key"
         )
 
+        '''
+        DATABASE
+        '''
         #TODO: Implement Postgres Status Table
 
-        # LAMBDAS
+        '''
+        LAMBDAS
+        '''
         intake_context_lambda = _lambda.DockerImageFunction(
             self, "IntakeContextLambda",
             code=_lambda.DockerImageCode.from_image_asset(
@@ -70,3 +81,29 @@ class AwsItAgenticAssistantStack(Stack):
                 "CLAUDE_API_KEY_SECRET_ARN": claude_secret.secret_arn
             }
         )
+
+        api_state_machine_lambda = _lambda.DockerImageFunction(
+            self, "ApiStateMachineLambda",
+            code=_lambda.DockerImageCode.from_image_asset(
+                directory="lambda/api_state_machine"
+            ),
+            architecture=_lambda.Architecture.ARM_64,
+            memory_size=256,
+            timeout=Duration.seconds(30),
+        )
+
+        '''
+        STEP FUNCTIONS: TASKS
+        '''
+        intake_context_task = None
+        ticketing_task = None
+        issue_resolution_preapr_task = None
+        issue_resolution_postapr_task = None
+
+        '''
+        STEP FUNCTIONS: STATE MACHINE
+        '''
+
+        '''
+        GRANTS AND ADDITIONAL SETUP
+        '''

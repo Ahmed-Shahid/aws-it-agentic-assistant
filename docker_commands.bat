7c98e775984f6994987749bba0f26e372e79493b
Ahmed Shahid <ahmed.s.shahid@gmail.com>
2026-08-18 10:36:43 -0700

    Modified docker_commands to reflect issue_resolution lambda

--- docker_commands.bat
+++ docker_commands.bat
@@ -4,11 +4,8 @@ docker build -t api_state_machine .
 cd ..\intake_context
 docker build -t intake_context .
 
-cd ..\issue_resolution_postapr
-docker build -t issue_resolution_postapr .
-
-cd ..\issue_resolution_preapr
-docker build -t issue_resolution_preapr .
+cd ..\issue_resolution
+docker build -t issue_resolution .
 
 cd ..\ticketing
 docker build -t ticketing .

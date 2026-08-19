a63cdfcdbaf34f8cf457dffcec4215baa4041ca2
Ahmed Shahid <ahmed.s.shahid@gmail.com>
2026-08-18 21:22:37 -0700

    Added stub API Tests

--- tests/api/api_test_commands.bat
+++ tests/api/api_test_commands.bat
@@ -1,2 +1,5 @@
 @REM API STATE MACHINE
-aws lambda invoke --function-name <ApiStateMachineFunctionName> --payload file://api_state_machine_event.json --cli-binary-format raw-in-base64-out out.json
+aws lambda invoke --function-name AwsItAgenticAssistantStac-ApiStateMachineLambdaD06-gMBoJ5RiKp1C --payload file://api_state_machine_event.json --cli-binary-format raw-in-base64-out out.json
+
+@REM DATA SEEDER
+aws lambda invoke --function-name <DATA SEEDER LAMBDA> --payload '{}' out.json

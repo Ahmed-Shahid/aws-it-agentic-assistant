2aec3d8f986e59c0b972cf64fc1c8ccfa735c8f6
Ahmed Shahid <ahmed.s.shahid@gmail.com>
2026-08-18 22:00:18 -0700

    Added cache to secrets.py

--- common/secrets.py
+++ common/secrets.py
@@ -1,16 +1,14 @@
 import json
-import os
-from contextlib import contextmanager
-
 import boto3
-from psycopg_pool import ConnectionPool
-from pgvector.psycopg import register_vector
 
 _secrets_client = boto3.client("secretsmanager")
+_cache = {}
 
 def get_secret(secret_arn: str, secret_key: str = None) -> str:
     """Fetches a secret value from AWS Secrets Manager."""
-    secret = json.loads(_secrets_client.get_secret_value(SecretId=secret_arn)["SecretString"])
+    if secret_arn not in _cache:
+        _cache[secret_arn] = json.loads(_secrets_client.get_secret_value(SecretId=secret_arn)["SecretString"])
+    secret = _cache[secret_arn]
     if secret_key is not None:
         return secret[secret_key]
     return secret

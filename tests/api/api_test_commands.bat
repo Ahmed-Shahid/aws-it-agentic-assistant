4e8e744b088cb26392df3e58f453f67f55eafa22
Ahmed Shahid <ahmed.s.shahid@gmail.com>
2026-08-19 21:03:05 -0700

    Added Data Seeder Lambda name for api test

--- tests/api/api_test_commands.bat
+++ tests/api/api_test_commands.bat
@@ -2,4 +2,4 @@
 aws lambda invoke --function-name AwsItAgenticAssistantStac-ApiStateMachineLambdaD06-gMBoJ5RiKp1C --payload file://api_state_machine_event.json --cli-binary-format raw-in-base64-out out.json
 
 @REM DATA SEEDER
-aws lambda invoke --function-name <DATA SEEDER LAMBDA> --payload '{}' out.json
+aws lambda invoke --function-name AwsItAgenticAssistantStac-DataSeederLambdaE9CA47BA-h04IHErleYjE --payload '{}' out.json

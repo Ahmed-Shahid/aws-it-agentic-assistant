5d9e33ec707c6857664ebc6e86d7ae249075b4fc
Ahmed-Shahid <ahmed.s.shahid@gmail.com>
2026-08-25 00:24:40 -0700

    Updated function names

--- tests/lambda/data_seeder/test_data_seeder.bat
+++ tests/lambda/data_seeder/test_data_seeder.bat
@@ -1,2 +1,2 @@
 @REM DATA SEEDER
-aws lambda invoke --function-name AwsItAgenticAssistantStac-DataSeederLambdaE9CA47BA-h04IHErleYjE --payload file://seed-event.json --cli-binary-format raw-in-base64-out out.json
+aws lambda invoke --function-name AwsItAgenticAssistantStack-TempQueryLambda01368E9B-1R5EVn8mwiDe --payload file://seed-event.json --cli-binary-format raw-in-base64-out out.json
--- tests/lambda/temp_query/test_temp_query.bat
+++ tests/lambda/temp_query/test_temp_query.bat
@@ -1,2 +1,2 @@
 @REM TEMP QUERY
-aws lambda invoke --function-name AwsItAgenticAssistantStack-TempQueryLambda01368E9B-XCsj0eUji5S7 --payload file://temp_query_event.json --cli-binary-format raw-in-base64-out out.json
+aws lambda invoke --function-name AwsItAgenticAssistantStack-TempQueryLambda01368E9B-1R5EVn8mwiDe --payload file://create_vector.json --cli-binary-format raw-in-base64-out out.json
